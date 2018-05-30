f37329449ad57a895f6065b29680bf7a9f6fdda3
root <root@clm-pun-t07dst.bmc.com>
2018-05-30 02:40:33 -0400

    Adding Docker image Build file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# build base docker image first
+docker build -t rajeshd2090/storage_base_image:latest -f Dockerfile_Base .
+#Build final image which inturn uses base image.
+# Here layers of base image will be used by final images and add only one layer of its one
+docker build -t rajeshd2090/storage_final_image:latest -f Dockerfile_Final .
+
